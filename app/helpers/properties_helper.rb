8d74d2e39b7eac1d14c46e425f9f6ef453835528
andrejbugi <andrejbugi96@gmail.com>
2021-01-06 22:53:10 +0100

    Show the account's agent when seeing the property, also list all the properties from the same agent, add better UI

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,10 +1,14 @@
 module PropertiesHelper
-  def property_thumbnail(property)
-    img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
-    image_tag img, class: "property-thumb"
+  def property_thumbnail(property, width = 300, height = 200)
+    img = property.photo.present? ? property.photo.thumb.url : 'placeholder.png'
+    image_tag img, width: width, height: height, class: 'property-thumb'
+  end
+
+  def property_thumbnail_url(property)
+    property.photo.present? ? property.photo.thumb.url : 'placeholder.png'
   end
 
   def property_photo_url(property)
-    property.photo.present? ? property.photo.url : asset_url("placeholder.png")
+    property.photo.present? ? property.photo.url : asset_url('placeholder.png')
   end
 end
