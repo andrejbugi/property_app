430c3f12054050abc112ccd6e61fd10d89d72006
andrejbugi <andrejbugi96@gmail.com>
2021-01-14 18:24:16 +0100

    Show property availability, show different things based on whether its for sale or for rent, show agent contact info

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     ENV = YAML::load_file('config/application.yml')
     config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'us-east-2'
+      provider:                 'AWS',
+      aws_access_key_id:        ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key:    ENV["AWS_SECRET_ACCESS_KEY"],
+      region:                   'us-east-2'
     }
     config.storage = :fog
     config.permissions = 0666
